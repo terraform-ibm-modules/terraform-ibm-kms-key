48b54c70b911dda96b0e95fad9d980e2abd441a5
Aashiq-J <aashiq.jacob@ibm.com>
2023-06-16 16:43:46 +0530

    fix: remove region

--- examples/existing-kms/variables.tf
+++ examples/existing-kms/variables.tf
@@ -4,12 +4,6 @@ variable "ibmcloud_api_key" {
   sensitive   = true
 }
 
-variable "region" {
-  type        = string
-  description = "Region to provision all resources created by this example"
-  default     = "us-south"
-}
-
 variable "prefix" {
   type        = string
   description = "Prefix to append to all resources created by this example"
