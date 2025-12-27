a8a372ee77b6f51a79319229e91f4042dfb17bf0
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-12-27 02:11:19 +0000

    fix(deps): update dependencies (#803)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -37,7 +37,7 @@ locals {
 module "secrets_manager_private_cert_engine" {
   count                     = var.existing_secrets_manager_crn == null && var.existing_cert_template_name == null ? 1 : 0
   source                    = "terraform-ibm-modules/secrets-manager-private-cert-engine/ibm"
-  version                   = "1.12.5"
+  version                   = "1.12.6"
   secrets_manager_guid      = module.sm_crn.service_instance
   region                    = var.region
   root_ca_name              = "${var.prefix}-ca"
@@ -52,7 +52,7 @@ module "secrets_manager_cert" {
   # no outputs from the private cert engine to reference in this module call
   depends_on             = [module.secrets_manager_private_cert_engine]
   source                 = "terraform-ibm-modules/secrets-manager-private-cert/ibm"
-  version                = "1.10.6"
+  version                = "1.10.7"
   secrets_manager_guid   = module.sm_crn.service_instance
   secrets_manager_region = module.sm_crn.region
   cert_name              = "${var.prefix}-kmip-cert"
